49ebaa63616508008a3a46ccc7470ce2fec56a7d
Aki Van Ness <aki@lethalbit.net>
2022-06-15 06:47:35 -0400

    etc: pylib: updated the Xylene Vivado wrapper

--- etc/pylibs/xylene/vivado.py
+++ etc/pylibs/xylene/vivado.py
@@ -16,11 +16,12 @@ __all__ = (
 
 class Vivado:
 	VIVADO_WORKING_DIR = (XYLENE_WORKING_DIR / 'vivado')
-	def __init__(self, *, log : bool = True, journal : bool = True, cwd : str = None, extra_args : list = ()) -> None:
+	def __init__(self, *, log : bool = True, journal : bool = True, cwd : str = None, extra_args : list = () , cleanup : bool = True) -> None:
 		if not self.VIVADO_WORKING_DIR.exists():
 			self.VIVADO_WORKING_DIR.mkdir()
 
 		self.run_dir = (self.VIVADO_WORKING_DIR / str(datetime.now().timestamp()))
+		self.cleanup = cleanup
 
 		self.vivado_args  = [
 			'vivado',
@@ -40,7 +41,20 @@ class Vivado:
 
 		self.cwd = cwd if cwd is not None else self.VIVADO_WORKING_DIR
 
-	def run(self, *, args : list) -> bool:
+	def __enter__(self):
+		return self
+
+	def __exit__(self, t, v, tb):
+		from shutil import rmtree
+
+		if not (t is None and v is None and tb is None):
+			return
+
+		if self.cleanup and self.run_dir.exists():
+			rmtree(self.run_dir)
+
+
+	def run(self, *, args : list, silent = True) -> bool:
 
 		log_args = []
 		if self.log:
@@ -58,18 +72,25 @@ class Vivado:
 
 		log.debug(f'Running \'{" ".join(run_list)}\'')
 
-		subprocess.run(
-			run_list,
-			stderr = subprocess.DEVNULL,
-			stdout = subprocess.DEVNULL,
-			cwd = str(self.cwd)
-		)
+		if not silent:
+			subprocess.run(
+				run_list,
+				cwd = str(self.cwd)
+			)
+		else:
+			subprocess.run(
+				run_list,
+				stderr = subprocess.DEVNULL,
+				stdout = subprocess.DEVNULL,
+				cwd = str(self.cwd)
+			)
 
-	def run_tcl(self, *, tcl_file, tcl_args):
+	def run_tcl(self, *, tcl_file, tcl_args, silent = True):
+
 		log.info(f'Running TCL file: \'{tcl_file}\'')
 		log.info(f'TCL Script args: {tcl_args}')
 		self.run(args = [
 			'-mode', 'batch',
 			'-source', tcl_file,
 			'-tclargs', *tcl_args
-		])
+		], silent = silent)
